0060905c1400c86c5da9fb6a1a5234ae177c5fad
Krishduttm <59798012+Krishduttm@users.noreply.github.com>
2022-02-13 15:26:23 +0530

    init

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,27 @@
+from brownie import (
+    accounts,
+    network,
+    config,
+    Contract,
+    interface,
+)
+from web3 import Web3
+from dotenv import load_dotenv
+
+load_dotenv()
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"][f"from_key"])
